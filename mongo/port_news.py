8bb9051c6d7499d871e3d62e3cadd40c3f190f4a
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-07-22 00:51:12 +0200

    initial commit

---
+++ mongo/port_news.py
@@ -0,0 +1,137 @@
+import json
+from datetime import datetime
+from pymongo import MongoClient
+import os
+import bson.binary
+
+date_format = '%d.%m.%Y %H:%M'
+
+# The is the file from mi-hp
+with open('home.json') as f:
+    data = json.load(f)    
+
+carouselnews = data["carouselmonitor"]
+data["carouselnews"] = []
+
+for n in carouselnews:
+    data["carouselnews"].append(
+        {
+            "test": True,
+            "_public": True, 
+            "start": datetime.strptime(n["showstart"], date_format),
+            "end": datetime.strptime(n["showend"], date_format),
+            "interval": int(n["interval"]),
+            "image_id": n["image"],
+            "left": int(n["left"].split("%")[0]),
+            "right": int(n["right"].split("%")[0]),
+            "bottom": int(n["bottom"].split("%")[0]),
+            "text": n["text"]
+        }
+    )
+
+
+news = data["news"]
+data["news"] = []
+
+for n in news:
+    # print(n)
+    data["news"].append(
+        {
+        "link": n["link"],
+        "image": [{
+            "_id": n["image"],
+            "stylehome": n["style"],
+            "stylemonitor": n["style"],
+            "widthmonitor": 5
+        }] if n["image"] != "" else [],
+        "home": {
+            "test": True, 
+            "_public": True,
+            "archiv": True,
+            "showlastday": True,
+            "start": datetime.strptime(n["showhomestart"], date_format),
+            "end": datetime.strptime(n["showhomeend"], date_format),
+            "title_de": n["title_de"],
+            "title_en": n["title_en"],
+            "text_de": n["text_de"],
+            "text_en": n["text_en"],
+            "popover_title_de": n["popover_title_de"],
+            "popover_title_en": n["popover_title_en"],
+            "popover_text_de": n["popover_text_de"],
+            "popover_text_en": n["popover_text_en"],
+        },
+        "monitor": {
+            "test": True, 
+            "_public": True,
+            "showlastday": True,
+            "start": datetime.strptime(n["showmonitorstart"], date_format),
+            "end": datetime.strptime(n["showmonitorend"], date_format),
+            "title": n["title_de"],
+            "text": n["text_de"]
+        }
+    })
+
+#with open('home2.json', 'w') as f:
+#    json.dump(data, f, ensure_ascii=False)    
+
+
+# Now we write data in the mongodb
+
+os.system("mongo news --eval 'db.dropDatabase()'")
+
+# Write to database:
+# Collections are: 
+# Mensaplan
+# Images: File, Titel, Bildnachweis, 
+# Carouselnews: test, _public, showstart, showend, interval, image, left (in %), right (in %), bottom (in %), text
+# News: wie oben
+
+try:
+    cluster = MongoClient("mongodb://127.0.0.1:27017")
+    mongo_db_news = cluster["news"]
+    mensaplan = mongo_db_news["mensaplan"]
+    bild = mongo_db_news["bild"]
+    carouselnews = mongo_db_news["carouselnews"]
+    news = mongo_db_news["news"]
+    
+except:
+    pass
+    # logger.warning("No connection to Database 1")
+
+for n in data["carouselnews"]:
+    im = bild.find_one({"filename" : n["image_id"]})
+    if im:
+        print(im["_id"])
+        n["image_id"] = im["_id"]
+    else:
+        print(n["image_id"])
+        with open(f'../../mi-hp{n["image_id"]}', 'rb') as image_file:
+            encoded_image = bson.binary.Binary(image_file.read())
+            newbild = bild.insert_one({"filename": n["image_id"], "mime": n["image_id"].split(".")[1].lower(), "data": encoded_image, "titel": "", "bildnachweis": ""})
+
+        n["image_id"] = newbild.inserted_id
+        
+    carouselnews.insert_one(n)
+
+for n in data["news"]:
+    if n["image"] != []:
+        im = bild.find_one({"filename" : n["image"][0]["_id"]})
+        if im:
+            print(im["_id"])
+            n["image"][0]["_id"] = im["_id"]
+        else:
+            print(n["image"][0]["_id"])
+            with open(f'../../mi-hp{n["image"][0]["_id"]}', 'rb') as image_file:
+                encoded_image = bson.binary.Binary(image_file.read())
+                newbild = bild.insert_one({"filename": n["image"][0]["_id"], "mime": n["image"][0]["_id"].split(".")[1].lower(), "data": encoded_image, "titel": "", "bildnachweis": ""})
+            n["image"][0]["_id"] = newbild.inserted_id
+    news.insert_one(n)
+
+
+print("Check schema")
+import schema_init
+
+mongo_db_news.command("collMod", "bild", validator = schema_init.bild_validator, validationLevel='moderate')
+mongo_db_news.command("collMod", "carouselnews", validator = schema_init.carouselnews_validator, validationLevel='moderate')
+mongo_db_news.command("collMod", "news", validator = schema_init.news_validator, validationLevel='moderate')
+
---
+++ mongo/schema_init.py
@@ -0,0 +1,264 @@
+from pymongo import MongoClient
+
+# Write to database:
+# Collections are: 
+# mensaplan
+# bild: File, Titel, Bildnachweis, 
+# carouselnews: test, _public, showstart, showend, interval, image, left (in %), right (in %), bottom (in %), text
+# news: link, image (object), home (object), monitor (object)
+
+
+# This is the mongodb
+cluster = MongoClient("mongodb://127.0.0.1:27017")
+mongo_db = cluster["news"]
+
+mensaplan_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "title": "Mensaessen eines Tages.",
+        "required": ["date", "text"],
+        "properties": {
+            "date": {
+                "bsonType": "date",
+                "description": "Datum, für das das Essen beschrieben wird. -- required"
+            },
+            "text": {
+                "bsonType": "array",
+                "items": {
+                    "bsonType": "string",
+                    "description": "eine Textzeile."
+                }
+            },
+        }
+    }
+}
+
+bild_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "title": "Ein Bild.",
+        "required": ["data", "mime", "filename", "titel", "bildnachweis"],
+        "properties": {
+            "data": {
+                "bsonType": "binData", 
+                "description": "Das File, wie es in der Datenbank abgespeichert wird -- required"
+            },
+            "mime": {
+                "bsonType": "string",
+                "description": "jpg, jpeg, png etc -- required"
+            },
+            "filename": {
+                "bsonType": "string",
+                "description": "Filename of the image -- required"
+            },
+            "titel": {
+                "bsonType": "string",
+                "description": "Ein Titel für das Bild -- required"
+            },
+            "bildnachweis": {
+                "bsonType": "string",
+                "description": "Der Bildnachweis -- required"
+            }
+        }
+    }
+}
+
+carouselnews_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "title": "Eine Nachricht für das Carousel.",
+        "required": ["test", "_public", "start", "end", "interval", "image_id", "left", "right", "bottom", "text"],
+        "properties": {
+            "test": {
+                "bsonType": "bool",
+                "description": "Gibt an, ob die News auf monitortest erscheint -- required"
+            },
+            "_public": {
+                "bsonType": "bool",
+                "description": "Gibt an, ob die News auf monitor erscheint -- required"
+            },
+            "start": {
+                "bsonType": "date",
+                "description": "Das Startdatum, zu dem die News angezeigt werden soll -- required"
+            },
+            "end": {
+                "bsonType": "date",
+                "description": "Das Enddatum, zu dem die News angezeigt werden soll -- required"
+            },
+            "interval": {
+                "bsonType": "int",
+                "description": "Die Dauer wie lange die News angezeigt wird in Milisekunden -- required"
+            },
+            "image_id": {
+                "bsonType": "objectId",
+                "description": "Das Hintergrundbild -- required"
+            },
+            "left": {
+                "bsonType": "int",
+                "minimum": 0,
+                "maximum": 100,
+                "description": "Entfernung der Box vom linken Rand (in %) -- required"
+            },
+            "right": {
+                "bsonType": "int",
+                "minimum": 0,
+                "maximum": 100,
+                "description": "Entfernung der Box vom rechten Rand (in %) -- required"
+            },
+            "bottom": {
+                "bsonType": "int",
+                "minimum": 0,
+                "maximum": 100,
+                "description": "Entfernung der Box vom unteren Rand (in %) -- required"
+            },
+            "text": {
+                "bsonType": "string",
+                "description": "Text im Bild -- required"
+            },
+        }
+    }
+}
+
+news_validator = {
+    "$jsonSchema": {
+        "bsonType": "object",
+        "title": "Eine Nachricht.",
+        "required": ["link", "image", "home", "monitor"],
+        "properties": {
+            "link": {
+                "bsonType": "string",
+                "description": "Gibt einen Link für diese News an (oder '') -- required"
+            },
+            "image": {
+                "bsonType": "array",
+                "description": "Das (wenn vorhanden) Bild für die News -- required. Das Array hat Länge 0 oder 1",
+                "items": {
+                    "bsonType": "object",
+                    "description": "Details eines Bildes",
+                    "required": ["_id", "stylehome", "stylemonitor", "widthmonitor"],
+                    "properties": {
+                        "_id": {
+                            "bsonType": "objectId",
+                            "description": "Die id des Bildes -- required"
+                        },
+                        "stylehome": {
+                            "bsonType": "string",
+                            "description": "css-Styles für das Bild auf home -- required"
+                        },
+                        "stylemonitor": {
+                            "bsonType": "string",
+                            "description": "css-Styles für das Bild auf monitor -- required"
+                        },
+                        "widthmonitor": {
+                            "bsonType": "int",
+                            "minimum": 0, 
+                            "maximum": 12,
+                            "description": "Breite im bootstrap-grid für das Bild auf monitor -- required"
+                        },                   
+                    },
+                },
+            },
+            "home": {
+                "bsonType": "object",
+                "description": "Details für home",
+                "required": ["test", "_public", "archiv", "showlastday", "start", "end", "title_de", "title_en", "text_de", "text_en", "popover_title_de", "popover_title_en", "popover_text_de", "popover_text_en"],
+                "properties": {
+                    "test": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob die News auf hometest zu sehen ist.  -- required"
+                    },
+                    "_public": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob die News auf home zu sehen ist.  -- required"
+                    },
+                    "archiv": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob die News nach end ins Archiv auf home übernommen wird.  -- required"
+                    },
+                    "showlastday": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob der letzte Tag speziell markiert werden soll. -- required"
+                    },
+                    "start": {
+                        "bsonType": "date",
+                        "description": "Startdatum für die News auf home -- required"
+                    },
+                    "end": {
+                        "bsonType": "date",
+                        "description": "Enddatum für die News auf home -- required"
+                    },
+                    "title_de": {
+                        "bsonType": "string",
+                        "description": "Titel der News auf home (de) -- required"
+                    },                   
+                    "title_en": {
+                        "bsonType": "string",
+                        "description": "Titel der News auf home (en) -- required"
+                    },                   
+                    "text_de": {
+                        "bsonType": "string",
+                        "description": "Text der News auf home (de) -- required"
+                    },                   
+                    "text_en": {
+                        "bsonType": "string",
+                        "description": "Text der News auf home (en) -- required"
+                    },                   
+                    "popover_title_de": {
+                        "bsonType": "string",
+                        "description": "Titel des Popovers der News auf home (de) -- required"
+                    },                   
+                    "popover_title_en": {
+                        "bsonType": "string",
+                        "description": "Titel des Popovers der News auf home (en) -- required"
+                    },                   
+                    "popover_text_de": {
+                        "bsonType": "string",
+                        "description": "Text des Popovers der News auf home (de) -- required"
+                    },                   
+                    "popover_text_en": {
+                        "bsonType": "string",
+                        "description": "Text des Popovers der News auf home (en) -- required"
+                    },                   
+                },
+            },
+            "monitor": {
+                "bsonType": "object",
+                "description": "Details für monitor",
+                "required": ["test", "_public", "showlastday", "start", "end", "title", "text"],
+                "properties": {
+                    "test": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob die News auf monitortest zu sehen ist.  -- required"
+                    },
+                    "_public": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob die News auf monitor zu sehen ist.  -- required"
+                    },
+                    "showlastday": {
+                        "bsonType": "bool",
+                        "description": "Gibt an, ob der letzte Tag speziell markiert werden soll. -- required"
+                    },
+                    "start": {
+                        "bsonType": "date",
+                        "description": "Startdatum für die News auf monitor -- required"
+                    },
+                    "end": {
+                        "bsonType": "date",
+                        "description": "Enddatum für die News auf monitor -- required"
+                    },
+                    "title": {
+                        "bsonType": "string",
+                        "description": "Titel der News auf monitor -- required"
+                    },                   
+                    "text": {
+                        "bsonType": "string",
+                        "description": "Text der News auf monitor -- required"
+                    },                   
+                },
+            },
+        }
+    }
+}
+
+
+
