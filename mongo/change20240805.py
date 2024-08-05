7784ef6994bc1267bb3103634884606e479dcb7a
pfaffelh <p.p@stochastik.uni-freiburg.de>
2024-08-05 21:28:56 +0200

    changed change

--- mongo/change20240805.py
+++ mongo/change20240805.py
@@ -3,16 +3,6 @@ import pymongo
 import os
 import datetime
 
-# Create mongodump 
-os.system(f"mongodump --db news --archive=news_backup_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}")
-
-# Delete complete database
-os.system("mongo news --eval 'db.dropDatabase()'")
-
-# Restore complete database
-# Should be executed from mi-vvz/mongo/
-os.system(f"mongorestore --archive='news_backup_{datetime.datetime.now().strftime('%Y%m%d_%H%M')}'")  
-
 cluster = MongoClient("mongodb://127.0.0.1:27017")
 mongo_db = cluster["news"]
 
@@ -20,7 +10,6 @@ bild = mongo_db["bild"]
 news = mongo_db["news"]
 carouselnews = mongo_db["carouselnews"]
 
-# Diesem Schema soll die Datenbank am Ende der Änderung folgen
 import schema20240805
 mongo_db.command('collMod','bild', validator=schema20240805.bild_validator, validationLevel='off')
 mongo_db.command('collMod','news', validator=schema20240805.news_validator, validationLevel='off')
@@ -29,7 +18,7 @@ mongo_db.command('collMod','carouselnews', validator=schema20240805.carouselnews
 # Ab hier wird die Datenbank verändert
 print("Ab hier wird verändert")
 
-# This gives a rang to 1. Vorlesung etc
+# Add fuerhome and fuermonitor
 ne = list(news.find())
 for n in ne:
     news.update_one({"_id" : n["_id"]}, { "$set" : {"monitor.fuermonitor" : True, "home.fuerhome" : True} })
