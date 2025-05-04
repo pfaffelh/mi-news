35659c16ea81b91f2cb7b59956587f8579374a42
pfaffelh <pfaffelh@gmail.com>
2025-05-04 14:45:51 +0200

    added doku

--- pages/08_Dokumentation.py
+++ pages/08_Dokumentation.py
@@ -23,13 +23,25 @@ tools.display_navigation()
 if st.session_state.logged_in:
     with st.expander("# Allgemeines", expanded=True):
         text = """
-        Diese App steuert die News und das Wochenprogramm auf der [Seite](https://www.math.uni-freiburg.de/nlehre/de/vortragsreihe/), sowie auf dem [Bildschirm im EG](https://www.math.uni-freiburg.de/nlehre/monitor/). Hier können
-                    
-        - Bilder in eine Datenbank geladen, 
-        - News (sowohl als Carouselnews oben im Monitor, als auch Einzelmeldungen) verfasst 
-        - Einträge ins Wochenprogramm vorgenommen
+        Diese App steuert das Wochenprogramm auf dieser [Seite](https://www.math.uni-freiburg.de/wochenprogramm/), sowie die News, die auf
+        * dem [Bildschirm im EG](https://www.math.uni-freiburg.de/nlehre/monitor/),
+        * der [Instituts-Homepage](https://www.math.uni-freiburg.de)
+        * der [Lehre-Homepage](https://www.math.uni-freiburg.de/nlehre/de/)
+        zu sehen sind.""" 
+        st.markdown(text)
+    with st.expander("# Wochenprogramm", expanded=False):
+        text = """
+        Für Einträge in das Wochenprogramm geht man so vor:
         
-        werden. Der übliche Workflow einer News sieht wiefolgt aus:  
+        - Auswahl der richtigen Vortragsreihe.
+        - Unter -> zu den Vorträgen -> Neuer Vortrag lässt sich ein neuer Vortrag anlegen. 
+        Ist man fertig und clickt auf -> Veröffentlicht, so ist der Vortrag unter
+        [Seite](https://www.math.uni-freiburg.de/wochenprogramm/) sichtbar.
+        """
+        st.markdown(text)
+    with st.expander("# News", expanded=False):
+        text = """        
+        Hier kann man sowohl Bilder in eine Datenbank laden, als auch News (sowohl als Carouselnews oben im Monitor, als auch Einzelmeldungen) verfassen. Der übliche Workflow einer News sieht wiefolgt aus:  
         
         - Eine News kommt herein, und man loggt sich in die App ein.  
         - Soll ein Bild mit angezeigt werden, braucht man ei entsprechendes jpg oder png, sowie eine Idee, ob die News auf dem Bildschirm und/oder der Homepage angezeigt werden soll.  
@@ -37,13 +49,6 @@ if st.session_state.logged_in:
         - Unter News -> Neue News anlegen bekommt man ein paar der möglichen Felder angezeigt. Hier ist _Veröffentlicht_ zunächst noch nicht angegeben, weil man ja erstmal die News ansehen will, bevor man sie veröffentlicht. Titel (das ist der Titel auf dem Monitor und der deutsche Titel auf der Homepage), Start/Enddatum der Anzeige lassen sich angeben, sowie das soeben angelegte Bild.  
         - Man schaut etwa [hier](https://www.math.uni-freiburg.de/nlehre/monitortest/), wie die News angezeigt wird, und kann sie dann den eigenen Wünschen folgend ändern.  
         - Ist man zufrieden, dann clickt man bei der News auf _Veröffentlicht_, und kann das Ergebnis [hier](https://www.math.uni-freiburg.de/nlehre/monitor/) ansehen.
-        
-        Für Einträge in das Wochenprogramm geht man so vor:
-        
-        - Auswahl der richtigen Vortragsreihe.
-        - Unter -> zu den Vorträgen -> Neuer Vortrag lässt sich ein neuer Vortrag anlegen. 
-        Ist man fertig und clickt auf -> Veröffentlicht, so ist der Vortrag unter
-        [Seite](https://www.math.uni-freiburg.de/nlehre/de/vortragsreihe/) sichtbar.
         """
         st.markdown(text)
         
